37a195bd69baf729c2ad1c4a2dd8056b34628a15
Maxime Fabre <ehtnam6@gmail.com>
2016-06-04 21:18:48 +0200

    Check if file exists

--- src/helpers/getFileContents.js
+++ src/helpers/getFileContents.js
@@ -1,5 +1,11 @@
 import fs from 'fs';
 
 export default function getFileContents(file) {
+    if (!fs.existsSync(file)) {
+        console.log('File "%s" does not exist'.red, file);
+
+        return false;
+    }
+
     return fs.readFileSync(file).toString();
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import path from 'path';
+import fs from 'fs';
 import program from 'commander';
 import 'colors';
 import packageInformations from '../package.json';
@@ -6,18 +7,22 @@ import msonToSchemas from './msonToSchemas';
 import schemasToFiles from './schemasToFiles';
 import {ensureDirectory, getFileContents} from './helpers';
 
-program
-    .version(packageInformations.version)
-    .usage('<input> <output>');
+program.version(packageInformations.version);
 
 program
-    .command('* <input> <output>')
+    .command('convert <input> <output>')
+    .alias('*')
     .description('Convert an MSON file into JSON schemas')
     .action((file, output) => {
         output = path.resolve(output);
         ensureDirectory(output);
 
+        // Get file contents
         const contents = getFileContents(file);
+        if (!contents) {
+            return process.exit(1);
+        }
+
         msonToSchemas(contents).then(schemas => {
             return schemasToFiles(output, schemas).forEach(schema => {
                 console.log('Created %s', schema.green);
